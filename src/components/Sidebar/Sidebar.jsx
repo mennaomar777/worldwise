35ba02ed08d5755f2f7ec157f2ed9725b89346a9
mennaomar777 <mennaomar764@gmail.com>
2025-08-23 00:00:54 +0300

    Fix import paths for AppNav

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Sidebar.module.css";
 import Logo from "../Logo/Logo";
-import AppNav from "../appNav/AppNav";
+import AppNav from "../AppNav/AppNav";
 import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
 export default function Sidebar() {
